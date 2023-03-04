8859d081614ef7cfe0959d402d79d092030ccdd3
puterbuagh <126985461+puterbuagh@users.noreply.github.com>
2023-03-05 06:32:01 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const board = document.createElement('div');
 board.className = 'board';
 
-for (let i = 0; i < 64; i++) {
-  const square = document.createElement('div');
-  square.className = 'square';
-  if (i % 2 === 0) {
-    square.classList.add('light');
-  } else {
-    square.classList.add('dark');
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    const square = document.createElement('div');
+    square.className = 'square';
+
+    // apply color based on row and column index
+    if ((i + j) % 2 === 0) {
+      square.classList.add('light');
+    } else {
+      square.classList.add('dark');
+    }
+
+    board.appendChild(square);
   }
-  board.appendChild(square);
 }
 
 document.body.appendChild(board);
