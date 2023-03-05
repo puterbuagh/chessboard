d523ca314d6c29ae79831e40f0491985c5a92c0b
puterbuagh <126985461+puterbuagh@users.noreply.github.com>
2023-03-05 16:15:19 +0800

    Update chessboard-setup.js

--- chessboard-setup.js
+++ chessboard-setup.js
@@ -33,3 +33,9 @@ export const piecePositions = {
   bP: {x: pieceWidth * 5, y: pieceHeight},
 };
 
+export function highlightSquare(row, col) {
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'rgba(255, 255, 0, 0.5)';
+  ctx.fillRect(col * squareSize, row * squareSize, squareSize, squareSize);
+}
+
