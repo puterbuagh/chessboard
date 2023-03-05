3e8a14ae0795953c3612fab000d410ab159412bb
puterbuagh <126985461+puterbuagh@users.noreply.github.com>
2023-03-06 06:38:28 +0800

    Update chessboard-setup.js

--- chessboard-setup.js
+++ chessboard-setup.js
@@ -19,23 +19,25 @@ export const pieceWidth = 50;
 export const pieceHeight = 50;
 
 export const piecePositions = {
-  wK: {x: 0, y: 0},
-  wQ: {x: pieceWidth, y: 0},
-  wR: {x: pieceWidth * 2, y: 0},
-  wB: {x: pieceWidth * 3, y: 0},
-  wN: {x: pieceWidth * 4, y: 0},
-  wP: {x: pieceWidth * 5, y: 0},
-  bK: {x: 0, y: pieceHeight},
-  bQ: {x: pieceWidth, y: pieceHeight},
-  bR: {x: pieceWidth * 2, y: pieceHeight},
-  bB: {x: pieceWidth * 3, y: pieceHeight},
-  bN: {x: pieceWidth * 4, y: pieceHeight},
-  bP: {x: pieceWidth * 5, y: pieceHeight},
+  wK: {x: pieceWidth * 4, y: pieceHeight * 7},
+  wQ: {x: pieceWidth * 3, y: pieceHeight * 7},
+  wR: {x: pieceWidth * 0, y: pieceHeight * 7},
+  wB: {x: pieceWidth * 2, y: pieceHeight * 7},
+  wN: {x: pieceWidth * 1, y: pieceHeight * 7},
+  wP: {x: pieceWidth * 5, y: pieceHeight * 6},
+  bK: {x: pieceWidth * 4, y: pieceHeight * 0},
+  bQ: {x: pieceWidth * 3, y: pieceHeight * 0},
+  bR: {x: pieceWidth * 0, y: pieceHeight * 0},
+  bB: {x: pieceWidth * 2, y: pieceHeight * 0},
+  bN: {x: pieceWidth * 1, y: pieceHeight * 0},
+  bP: {x: pieceWidth * 5, y: pieceHeight * 1},
 };
+
 
 export function highlightSquare(row, col, color, canvas) {
   const ctx = canvas.getContext('2d');
   ctx.fillStyle = color;
   ctx.fillRect(col * squareSize, row * squareSize, squareSize, squareSize);
 }
+
 
