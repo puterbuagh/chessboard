1810a7af30de0d456a76be6a7e9df913fac5fb98
puterbuagh <126985461+puterbuagh@users.noreply.github.com>
2023-03-06 14:20:47 +0800

    Update chessboard-setup.js

--- chessboard-setup.js
+++ chessboard-setup.js
@@ -1,18 +1,19 @@
 export const squareSize = 50;
 
 export const pieces = {
-  wK: "/img/pieces/wK.png",
-  wQ: "/img/pieces/wQ.png",
-  wR: "/img/pieces/wR.png",
-  wB: "/img/pieces/wB.png",
-  wN: "/img/pieces/wN.png",
-  wP: "/img/pieces/wP.png",
-  bK: "/img/pieces/bK.png",
-  bQ: "/img/pieces/bQ.png",
-  bR: "/img/pieces/bR.png",
-  bB: "/img/pieces/bB.png",
-  bN: "/img/pieces/bN.png",
-  bP: "/img/pieces/bP.png",
+  wK: "img/pieces/wK.png",
+wQ: "img/pieces/wQ.png",
+wR: "img/pieces/wR.png",
+wB: "img/pieces/wB.png",
+wN: "img/pieces/wN.png",
+wP: "img/pieces/wP.png",
+bK: "img/pieces/bK.png",
+bQ: "img/pieces/bQ.png",
+bR: "img/pieces/bR.png",
+bB: "img/pieces/bB.png",
+bN: "img/pieces/bN.png",
+bP: "img/pieces/bP.png",
+
 };
 
 export const pieceWidth = 50;
