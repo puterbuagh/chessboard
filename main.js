98492381806fc34894139d78f15330f229012e4e
puterbuagh <126985461+puterbuagh@users.noreply.github.com>
2023-03-08 14:22:14 +0800

    Update main.js

--- main.js
+++ main.js
@@ -36,8 +36,6 @@ canvas.width = 400;
 canvas.height = 400;
 document.body.appendChild(canvas);
 
-const ctx = canvas.getContext('2d');
-
 const whiteKingImg = 'img/pieces/wK.png';
 const whiteQueenImg = 'img/pieces/wQ.png';
 const whiteRookImg = 'img/pieces/wR.png';
